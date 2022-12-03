bb555fff08cc60a26bd32bc5025f99df1c244288
Jason A. Fager <jafager@jafager.com>
2022-12-02 21:34:29 -0500

    Initial import of keepalived and haproxy.

--- kadmlab/kadmlab.tf
+++ kadmlab/kadmlab.tf
@@ -16,6 +16,12 @@ variable "node_count" {
     default = 6
 }
 
+variable "lb_count" {
+    type = number
+    description = "The number of load balancers to create."
+    default = 2
+}
+
 resource "libvirt_network" "kadmlab_management" {
     name = "kadmlab_management"
     mode = "nat"
@@ -54,8 +60,16 @@ resource "libvirt_network" "kadmlab_management" {
             ip = "192.168.251.16"
         }
         hosts {
+            hostname = "kadmlablb1"
+            ip = "192.168.251.21"
+        }
+        hosts {
+            hostname = "kadmlablb2"
+            ip = "192.168.251.22"
+        }
+        hosts {
             hostname = "controlplane"
-            ip = "192.168.252.11"
+            ip = "192.168.251.100"
         }
     }
     dhcp {
@@ -105,11 +119,28 @@ resource "libvirt_volume" "kadmlabnode" {
     depends_on = [libvirt_volume.ubuntu2204]
 }
 
+resource "libvirt_volume" "kadmlablb" {
+    count = var.lb_count
+    name = "kadmlablb${count.index + 1}.qcow2"
+    pool = "kadmlab"
+    format = "qcow2"
+    size = 16 * 1024 * 1024 * 1024
+    base_volume_name = "ubuntu2204.qcow2"
+    depends_on = [libvirt_volume.ubuntu2204]
+}
+
 resource "libvirt_cloudinit_disk" "kadmlabnode" {
     count = var.node_count
     name = "kadmlabnode${count.index + 1}.iso"
     user_data = templatefile("${path.module}/kadmlabnode_user_data.tftpl", { hostname = "kadmlabnode${count.index + 1}", fqdn = "kadmlabnode${count.index + 1}.kadmlab.prism.local", username = "jafager", password = "Ubuntu22.04", ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9My5YdUN2wC+FC90bSCF6t3jrMGESv7+eafQETfD3t1bCwoPHpXywzeP6qVfgCxQQa7mmBT5sEOrJjtVw02QQA8C7vca+rprMOrN6rooCAZDlt8whoARv++MjgMnBset2QxuL5OoTOkLmVdw0rTubbxqf80CjZL/T7DVD04sS9CEQQOY7Qb9IzPtykATvKzalWLqT7GJNx+oMghGbAjx/AO4KyEwFgAEeT6vd72AtwsS7PKv46dL44IQSEg1T3Z6HVW0sF0/w9VMcujgwvBuveLGoRuH2kiniWiYSBOylbuTu1SMnKMHsJHm11aMqhnhZCB9fZPOdjYBuUZOMa0hiWq3WXq11Q+UMJurcQTFlB1bZf9ZBPwJzERiw4Z1eZwBC5JucAtajQYpsjRKskCKdixpkJ5l9oinKwfjYCnj+SjE+GeT7on3QA8iCtR4WCixdFVYszkfOX1SPd3mzRdohIjgAg3rWxOP/jkEXUm289F/uKAQUg5bIzIEJNAIfNWU= jafager@prism" })
     network_config = templatefile("${path.module}/kadmlabnode_network_config.tftpl", { ip_addresses_1 = "[192.168.252.${10 + (count.index + 1)}/24]", gateway = "192.168.252.1", nameservers = "[192.168.252.1]", domains = "[kadmlab.prism.local]", ip_addresses_2 = "[192.168.251.${10 + (count.index + 1)}/24]", ip_addresses_3 = "[192.168.250.${10 + (count.index + 1)}/24]"})
+}
+
+resource "libvirt_cloudinit_disk" "kadmlablb" {
+    count = var.lb_count
+    name = "kadmlablb${count.index + 1}.iso"
+    user_data = templatefile("${path.module}/kadmlabnode_user_data.tftpl", { hostname = "kadmlablb${count.index + 1}", fqdn = "kadmlablb${count.index + 1}.kadmlab.prism.local", username = "jafager", password = "Ubuntu22.04", ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9My5YdUN2wC+FC90bSCF6t3jrMGESv7+eafQETfD3t1bCwoPHpXywzeP6qVfgCxQQa7mmBT5sEOrJjtVw02QQA8C7vca+rprMOrN6rooCAZDlt8whoARv++MjgMnBset2QxuL5OoTOkLmVdw0rTubbxqf80CjZL/T7DVD04sS9CEQQOY7Qb9IzPtykATvKzalWLqT7GJNx+oMghGbAjx/AO4KyEwFgAEeT6vd72AtwsS7PKv46dL44IQSEg1T3Z6HVW0sF0/w9VMcujgwvBuveLGoRuH2kiniWiYSBOylbuTu1SMnKMHsJHm11aMqhnhZCB9fZPOdjYBuUZOMa0hiWq3WXq11Q+UMJurcQTFlB1bZf9ZBPwJzERiw4Z1eZwBC5JucAtajQYpsjRKskCKdixpkJ5l9oinKwfjYCnj+SjE+GeT7on3QA8iCtR4WCixdFVYszkfOX1SPd3mzRdohIjgAg3rWxOP/jkEXUm289F/uKAQUg5bIzIEJNAIfNWU= jafager@prism" })
+    network_config = templatefile("${path.module}/kadmlabnode_network_config.tftpl", { ip_addresses_1 = "[192.168.252.${20 + (count.index + 1)}/24]", gateway = "192.168.252.1", nameservers = "[192.168.252.1]", domains = "[kadmlab.prism.local]", ip_addresses_2 = "[192.168.251.${20 + (count.index + 1)}/24]", ip_addresses_3 = "[192.168.250.${20 + (count.index + 1)}/24]"})
 }
 
 resource "libvirt_domain" "kadmlabnode" {
@@ -131,4 +162,25 @@ resource "libvirt_domain" "kadmlabnode" {
     }
     cloudinit = libvirt_cloudinit_disk.kadmlabnode[count.index].id
     depends_on = [libvirt_volume.kadmlabnode, libvirt_network.kadmlab_management, libvirt_network.kadmlab_cluster, libvirt_network.kadmlab_service]
+}
+
+resource "libvirt_domain" "kadmlablb" {
+    count = var.lb_count
+    name = "kadmlablb${count.index + 1}"
+    vcpu = "1"
+    memory = "4096"
+    disk {
+        volume_id = libvirt_volume.kadmlablb[count.index].id
+    }
+    network_interface {
+        network_id = libvirt_network.kadmlab_management.id
+    }
+    network_interface {
+        network_id = libvirt_network.kadmlab_cluster.id
+    }
+    network_interface {
+        network_id = libvirt_network.kadmlab_service.id
+    }
+    cloudinit = libvirt_cloudinit_disk.kadmlablb[count.index].id
+    depends_on = [libvirt_volume.kadmlablb, libvirt_network.kadmlab_management, libvirt_network.kadmlab_cluster, libvirt_network.kadmlab_service]
 }
