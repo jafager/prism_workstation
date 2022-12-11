01904152396184c2ac4b4ca856488f974d9c3f25
Jason A. Fager <jafager@jafager.com>
2022-12-11 13:02:25 -0500

    Added metallb network.

--- kadmlab/kadmlab.tf
+++ kadmlab/kadmlab.tf
@@ -81,6 +81,9 @@ resource "libvirt_network" "kadmlab_cluster" {
     name = "kadmlab_cluster"
     mode = "none"
     addresses = ["192.168.251.0/24"]
+    dns {
+        enabled = false
+    }
     dhcp {
         enabled = false
     }
@@ -90,6 +93,21 @@ resource "libvirt_network" "kadmlab_service" {
     name = "kadmlab_service"
     mode = "none"
     addresses = ["192.168.250.0/24"]
+    dns {
+        enabled = false
+    }
+    dhcp {
+        enabled = false
+    }
+}
+
+resource "libvirt_network" "kadmlab_metallb" {
+    name = "kadmlab_metallb"
+    mode = "none"
+    addresses = ["192.168.249.0/24"]
+    dns {
+        enabled = false
+    }
     dhcp {
         enabled = false
     }
@@ -133,14 +151,14 @@ resource "libvirt_cloudinit_disk" "kadmlabnode" {
     count = var.node_count
     name = "kadmlabnode${count.index + 1}.iso"
     user_data = templatefile("${path.module}/kadmlabnode_user_data.tftpl", { hostname = "kadmlabnode${count.index + 1}", fqdn = "kadmlabnode${count.index + 1}.kadmlab.prism.local", username = "jafager", password = "Ubuntu22.04", ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9My5YdUN2wC+FC90bSCF6t3jrMGESv7+eafQETfD3t1bCwoPHpXywzeP6qVfgCxQQa7mmBT5sEOrJjtVw02QQA8C7vca+rprMOrN6rooCAZDlt8whoARv++MjgMnBset2QxuL5OoTOkLmVdw0rTubbxqf80CjZL/T7DVD04sS9CEQQOY7Qb9IzPtykATvKzalWLqT7GJNx+oMghGbAjx/AO4KyEwFgAEeT6vd72AtwsS7PKv46dL44IQSEg1T3Z6HVW0sF0/w9VMcujgwvBuveLGoRuH2kiniWiYSBOylbuTu1SMnKMHsJHm11aMqhnhZCB9fZPOdjYBuUZOMa0hiWq3WXq11Q+UMJurcQTFlB1bZf9ZBPwJzERiw4Z1eZwBC5JucAtajQYpsjRKskCKdixpkJ5l9oinKwfjYCnj+SjE+GeT7on3QA8iCtR4WCixdFVYszkfOX1SPd3mzRdohIjgAg3rWxOP/jkEXUm289F/uKAQUg5bIzIEJNAIfNWU= jafager@prism" })
-    network_config = templatefile("${path.module}/kadmlabnode_network_config.tftpl", { ip_addresses_1 = "[192.168.252.${10 + (count.index + 1)}/24]", gateway = "192.168.252.1", nameservers = "[192.168.252.1]", domains = "[kadmlab.prism.local]", ip_addresses_2 = "[192.168.251.${10 + (count.index + 1)}/24]", ip_addresses_3 = "[192.168.250.${10 + (count.index + 1)}/24]"})
+    network_config = templatefile("${path.module}/kadmlabnode_network_config.tftpl", { ip_addresses_1 = "[192.168.252.${10 + (count.index + 1)}/24]", gateway = "192.168.252.1", nameservers = "[192.168.252.1]", domains = "[kadmlab.prism.local]", ip_addresses_2 = "[192.168.251.${10 + (count.index + 1)}/24]", ip_addresses_3 = "[192.168.250.${10 + (count.index + 1)}/24]", ip_addresses_4 = "[192.168.249.${20 + (count.index + 1)}/24]" })
 }
 
 resource "libvirt_cloudinit_disk" "kadmlablb" {
     count = var.lb_count
     name = "kadmlablb${count.index + 1}.iso"
-    user_data = templatefile("${path.module}/kadmlabnode_user_data.tftpl", { hostname = "kadmlablb${count.index + 1}", fqdn = "kadmlablb${count.index + 1}.kadmlab.prism.local", username = "jafager", password = "Ubuntu22.04", ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9My5YdUN2wC+FC90bSCF6t3jrMGESv7+eafQETfD3t1bCwoPHpXywzeP6qVfgCxQQa7mmBT5sEOrJjtVw02QQA8C7vca+rprMOrN6rooCAZDlt8whoARv++MjgMnBset2QxuL5OoTOkLmVdw0rTubbxqf80CjZL/T7DVD04sS9CEQQOY7Qb9IzPtykATvKzalWLqT7GJNx+oMghGbAjx/AO4KyEwFgAEeT6vd72AtwsS7PKv46dL44IQSEg1T3Z6HVW0sF0/w9VMcujgwvBuveLGoRuH2kiniWiYSBOylbuTu1SMnKMHsJHm11aMqhnhZCB9fZPOdjYBuUZOMa0hiWq3WXq11Q+UMJurcQTFlB1bZf9ZBPwJzERiw4Z1eZwBC5JucAtajQYpsjRKskCKdixpkJ5l9oinKwfjYCnj+SjE+GeT7on3QA8iCtR4WCixdFVYszkfOX1SPd3mzRdohIjgAg3rWxOP/jkEXUm289F/uKAQUg5bIzIEJNAIfNWU= jafager@prism" })
-    network_config = templatefile("${path.module}/kadmlabnode_network_config.tftpl", { ip_addresses_1 = "[192.168.252.${20 + (count.index + 1)}/24]", gateway = "192.168.252.1", nameservers = "[192.168.252.1]", domains = "[kadmlab.prism.local]", ip_addresses_2 = "[192.168.251.${20 + (count.index + 1)}/24]", ip_addresses_3 = "[192.168.250.${20 + (count.index + 1)}/24]"})
+    user_data = templatefile("${path.module}/kadmlablb_user_data.tftpl", { hostname = "kadmlablb${count.index + 1}", fqdn = "kadmlablb${count.index + 1}.kadmlab.prism.local", username = "jafager", password = "Ubuntu22.04", ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9My5YdUN2wC+FC90bSCF6t3jrMGESv7+eafQETfD3t1bCwoPHpXywzeP6qVfgCxQQa7mmBT5sEOrJjtVw02QQA8C7vca+rprMOrN6rooCAZDlt8whoARv++MjgMnBset2QxuL5OoTOkLmVdw0rTubbxqf80CjZL/T7DVD04sS9CEQQOY7Qb9IzPtykATvKzalWLqT7GJNx+oMghGbAjx/AO4KyEwFgAEeT6vd72AtwsS7PKv46dL44IQSEg1T3Z6HVW0sF0/w9VMcujgwvBuveLGoRuH2kiniWiYSBOylbuTu1SMnKMHsJHm11aMqhnhZCB9fZPOdjYBuUZOMa0hiWq3WXq11Q+UMJurcQTFlB1bZf9ZBPwJzERiw4Z1eZwBC5JucAtajQYpsjRKskCKdixpkJ5l9oinKwfjYCnj+SjE+GeT7on3QA8iCtR4WCixdFVYszkfOX1SPd3mzRdohIjgAg3rWxOP/jkEXUm289F/uKAQUg5bIzIEJNAIfNWU= jafager@prism" })
+    network_config = templatefile("${path.module}/kadmlablb_network_config.tftpl", { ip_addresses_1 = "[192.168.252.${20 + (count.index + 1)}/24]", gateway = "192.168.252.1", nameservers = "[192.168.252.1]", domains = "[kadmlab.prism.local]", ip_addresses_2 = "[192.168.251.${20 + (count.index + 1)}/24]" })
 }
 
 resource "libvirt_domain" "kadmlabnode" {
@@ -160,8 +178,11 @@ resource "libvirt_domain" "kadmlabnode" {
     network_interface {
         network_id = libvirt_network.kadmlab_service.id
     }
+    network_interface {
+        network_id = libvirt_network.kadmlab_metallb.id
+    }
     cloudinit = libvirt_cloudinit_disk.kadmlabnode[count.index].id
-    depends_on = [libvirt_volume.kadmlabnode, libvirt_network.kadmlab_management, libvirt_network.kadmlab_cluster, libvirt_network.kadmlab_service]
+    depends_on = [libvirt_volume.kadmlabnode, libvirt_network.kadmlab_management, libvirt_network.kadmlab_cluster, libvirt_network.kadmlab_service, libvirt_network.kadmlab_metallb]
 }
 
 resource "libvirt_domain" "kadmlablb" {
@@ -178,9 +199,6 @@ resource "libvirt_domain" "kadmlablb" {
     network_interface {
         network_id = libvirt_network.kadmlab_cluster.id
     }
-    network_interface {
-        network_id = libvirt_network.kadmlab_service.id
-    }
     cloudinit = libvirt_cloudinit_disk.kadmlablb[count.index].id
-    depends_on = [libvirt_volume.kadmlablb, libvirt_network.kadmlab_management, libvirt_network.kadmlab_cluster, libvirt_network.kadmlab_service]
+    depends_on = [libvirt_volume.kadmlablb, libvirt_network.kadmlab_management, libvirt_network.kadmlab_cluster]
 }
