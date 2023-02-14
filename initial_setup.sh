bd3376f716d72efab222b0ab6c0bb424c308f471
Jason A. Fager <jafager@jafager.com>
2023-02-14 19:16:22 +0000

    Added script for initial setup.

---
+++ initial_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# Upgrade packages
+sudo apt update
+sudo apt -y Upgrade
+
+# Install Ansible
+sudo apt -y install software-properties-common
+sudo apt-repository-add -y ppa:ansible/ansible
+sudo apt update
+sudo apt -y install ansible
+
+# Install desktop environment
+sudo apt -y install ubuntu-desktop-minimal
+
+# Install Nvidia driver
+sudo apt -y install nvidia-driver-525
+
+# Install Chromium
+sudo snap install chromium
+
+# Install Visual Studio Code
+sudo snap install --classic Code
